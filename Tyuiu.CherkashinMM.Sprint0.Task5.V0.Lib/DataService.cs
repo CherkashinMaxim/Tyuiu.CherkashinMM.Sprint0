d1704a336a2aed532ca79f7a6dee2ca109afc2a8
CherkashinMaxim <cherkashin.maxim1@gmail.com>
2024-09-11 21:16:35 +0500

    Task5

---
+++ Tyuiu.CherkashinMM.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.CherkashinMM.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.CherkashinMM.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Subtraction(int num1, int num2)
+        {
+            return (num1 - num2);
+        }
+        public static int Multipl(int num1, int num2)
+        {
+            return (num1 * num2);
+        }
+        public static int Division(int num1, int num2)
+        {
+            if (num2 == 0) {
+                Console.WriteLine("Переменная num2 = {0}. На ноль делить нельзя", num2);
+                return -1;
+            }
+            return (num1 / num2);
+        }
+    }
+}
---
+++ Tyuiu.CherkashinMM.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.CherkashinMM.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.CherkashinMM.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("num1 + num2 = " + DataService.Addition(1,5));
+            Console.WriteLine("num1 - num2 = " + DataService.Subtraction(15,5));
+            Console.WriteLine("num1 * num2 = " + DataService.Multipl(10, 10));
+            Console.WriteLine("num1 / num2 = " + DataService.Division(9,3));
+            Console.ReadKey();
+        }
+    }
+}
