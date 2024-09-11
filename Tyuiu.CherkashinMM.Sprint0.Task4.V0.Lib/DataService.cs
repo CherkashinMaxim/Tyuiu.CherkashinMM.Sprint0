58481fdca27b617aa1a29ed4d8b920869b742f6e
CherkashinMaxim <cherkashin.maxim1@gmail.com>
2024-09-11 20:50:43 +0500

    Task4

---
+++ Tyuiu.CherkashinMM.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.CherkashinMM.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Subtraction(int num1, int num2)
+        {
+            return (num1 - num2);
+        }
+        public static int Multipl(int num1, int num2)
+        {
+            return (num1 * num2);
+        }
+        public static int Division(int num1, int num2)
+        {
+            return (num1 / num2);
+        }
+    }
+}
---
+++ Tyuiu.CherkashinMM.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.CherkashinMM.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.CherkashinMM.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        [TestMethod]
+        public void CheckSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        [TestMethod]
+        public void CheckMultiplValid()
+        {
+            Assert.AreEqual(50, DataService.Multipl(10, 5));
+        }
+        [TestMethod]
+        public void CheckDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+
+    }
+}
