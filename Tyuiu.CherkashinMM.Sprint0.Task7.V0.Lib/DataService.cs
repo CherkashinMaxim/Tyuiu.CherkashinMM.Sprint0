d14341b1b7dd09bd2b1fb1e5290a528ff44ea116
CherkashinMaxim <cherkashin.maxim1@gmail.com>
2024-09-12 11:56:28 +0500

    Task7

--- Tyuiu.CherkashinMM.Sprint0.Task7.V0.Lib/DataService.cs
+++ Tyuiu.CherkashinMM.Sprint0.Task7.V0.Lib/DataService.cs
@@ -1,7 +1,17 @@
-﻿namespace Tyuiu.CherkashinMM.Sprint0.Task7.V0.Lib
+﻿using System.Globalization;
+
+namespace Tyuiu.CherkashinMM.Sprint0.Task7.V0.Lib
 {
     public class DataService
     {
-
+        public static int[] AdditionArrays(int[] nums1, int[] nums2)
+        {
+            int[] res = new int[5];
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                res[i] = nums1[i]+nums2[i];
+            }
+            return res;
+        }
     }
 }
